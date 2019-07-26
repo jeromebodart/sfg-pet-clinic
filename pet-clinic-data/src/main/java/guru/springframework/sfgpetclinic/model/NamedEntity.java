0ec92f4afa3572b9b488a9711300e73a7ffa7744
mortonne <jerome.bodart@hei.yncrea.fr>
2019-07-26 14:15:45 +0200

    Updated owner with JPA entity mappings. Closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/NamedEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/NamedEntity.java
@@ -1,7 +1,12 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class NamedEntity extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
