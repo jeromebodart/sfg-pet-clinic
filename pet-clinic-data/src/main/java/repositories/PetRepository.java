61d80e5b3f073ed0f0494240d444daf44919d9d7
mortonne <jerome.bodart@hei.yncrea.fr>
2019-07-26 14:44:57 +0200

    implemented repo . Closes #40

---
+++ pet-clinic-data/src/main/java/repositories/PetRepository.java
@@ -0,0 +1,7 @@
+package repositories;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetRepository extends CrudRepository<Pet, Long> {
+}
